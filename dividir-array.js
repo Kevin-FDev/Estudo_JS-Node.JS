949f6447045086c657c723d234d72c0c490dacb1
Kevin-FDev <kevin.2006.rf@gmail.com>
2026-02-04 17:35:11 -0300

    aula de .slice

---
+++ dividir-array.js
@@ -0,0 +1,8 @@
+const listarEstudantes = ['Kevin', 'Juliana', 'Ana', 'Aline', 'Bianca', 'Isabela', 'Deise', 'Luiza', 'Brenda', 'Duda', 'Jaqueline', 'Lauren'];
+
+const sala1 = listarEstudantes.slice(0, listarEstudantes.length/2);
+
+const sala2 = listarEstudantes.slice(listarEstudantes.length/2);
+
+console.log(sala1);
+console.log(sala2);
